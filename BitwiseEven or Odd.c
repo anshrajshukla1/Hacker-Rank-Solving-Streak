22c6f519847d0172150c474bf302bfee9c0e54cd
Ansh Raj Shukla <anshrajshukla.official@gmail.com>
2025-02-10 23:08:28 +0530

    Solution to #Q18

---
+++ BitwiseEven or Odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    
+    scanf("%d", &N);
+
+    
+    if (N & 1) {
+    
+        printf("Odd\n");
+    } else {
+
+        printf("Even\n");
+    }
+
+    return 0;
+}
